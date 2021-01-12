12635beb861daf2ff9b06f8ed277ea01ee3c0b78
sunkey <614053620@qq.com>
2021-01-12 10:08:35 +0800

    java11&openjfx

---
+++ bilibili-jfx/src/main/java/tech/sunkey/bilibili/jfx/JfxApp.java
@@ -0,0 +1,21 @@
+package tech.sunkey.bilibili.jfx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Sunkey
+ * @since 2021-01-12 9:00 上午
+ **/
+public class JfxApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
