3a8130ab28c4191a35e371092e52b3c5c3828c18
sunkey <614053620@qq.com>
2021-01-12 22:14:40 +0800

    add jfx deps

--- bilibili-jfx/src/main/java/tech/sunkey/bilibili/jfx/FXApp.java
+++ bilibili-jfx/src/main/java/tech/sunkey/bilibili/jfx/FXApp.java
@@ -1,9 +1,12 @@
 package tech.sunkey.bilibili.jfx;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import tech.sunkey.bilibili.jfx.view.MainController;
 
 /**
  * @author Sunkey
@@ -13,8 +16,9 @@ public class FXApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        BorderPane root = new BorderPane();
-        Scene scene = new Scene(root, 400,400);
+        Parent root = FXMLLoader.load(MainController.class.getResource("MainView.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("FXAPP");
         stage.setScene(scene);
         stage.show();
     }
