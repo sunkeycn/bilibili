555f5839da105b3ee4255e158990d9b890b6e4b2
sunkey <614053620@qq.com>
2021-01-12 21:11:35 +0800

    remove 3rd party deps

--- bilibili-platform/src/test/java/tech/sunkey/bilibili/tests/BeanGen.java
+++ bilibili-platform/src/test/java/tech/sunkey/bilibili/tests/BeanGen.java
@@ -1,8 +1,5 @@
 package tech.sunkey.bilibili.tests;
 
-import com.onway.common.utils.FileUtils;
-import com.onway.common.utils.dev.JsonBean;
-
 /**
  * @author Sunkey
  * @since 2021-01-11 3:02 下午
@@ -10,9 +7,9 @@ import com.onway.common.utils.dev.JsonBean;
 public class BeanGen {
 
     public static void main(String[] args) {
-        String gen = new JsonBean().generate("OnlineRankV2",
-                FileUtils.from("classpath:orv2.json").readToString());
-        System.out.println(gen);
+        //String gen = new JsonBean().generate("OnlineRankV2",
+        //        FileUtils.from("classpath:orv2.json").readToString());
+        //System.out.println(gen);
     }
 
 }
--- bilibili-ws-client/src/test/java/tech/sunkey/bilibili/ws/tests/Json2Bean.java
+++ bilibili-ws-client/src/test/java/tech/sunkey/bilibili/ws/tests/Json2Bean.java
@@ -1,7 +1,5 @@
 package tech.sunkey.bilibili.ws.tests;
 
-import com.onway.common.utils.FileUtils;
-import com.onway.common.utils.dev.JsonBean;
 
 /**
  * @author Sunkey
@@ -10,8 +8,8 @@ import com.onway.common.utils.dev.JsonBean;
 public class Json2Bean {
 
     public static void main(String[] args) {
-        String gen = new JsonBean().generate("UserAuth", FileUtils.from("classpath:UserAuth.json").readToString());
-        System.out.println(gen);
+        //String gen = new JsonBean().generate("UserAuth", FileUtils.from("classpath:UserAuth.json").readToString());
+        //System.out.println(gen);
     }
 
 }
